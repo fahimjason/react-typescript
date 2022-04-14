0d8a635be0f6ae90acc55be77b273dd986496637
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-14 11:51:44 +0600

    03-types-around-events-and-refs

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom';
+import UserSearch from './refs/UserSearch';
 
 const App = () => {
   return (
     <div>
-      <h1>Hi there!</h1>
+      <UserSearch />
     </div>
   );
 };
